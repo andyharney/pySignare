3fbd3db93fecf65565cc4aa632913babb3295383
Andy <andy.harney@andyharney.co.uk>
2014-02-22 18:19:09 +0000

    Big Cleanup
    Lots of clean up & reordered the functions.
    Added comments on what is happening.
    Cleared out the objects after use.

--- Main.py
+++ Main.py
@@ -12,104 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# Here we go
-# Check to see if Java is installed and added to system path
-
-
-def checkjava():
-
-    import sys
-    import subprocess
-
-    sjava = subprocess.call(['java', '-version'])
-    if sjava is None:
-        print('Java not installed or added to system path, please install')
-        pause = input('Press Any Key To Exit')
-        del pause
-        # Exit gracefully
-        sys.exit()
-    else:
-        print()
-        checkoutputfolder()
-
-
-def mainmenu():
-
-    linebreak = ('-' * 28)
-    print('Main Menu')
-    print(linebreak)
-    print('1 : Sign With Debug Key')
-    print('2 : Sign With Private Key')
-    print('3 : ZipAlign Signed APKs')
-    print(linebreak)
-    print('4 : Generate Private Key')
-    print(linebreak)
-    print('5 : Exit ')
-    print(linebreak)
-    menuchoice()
-
-
-def menuchoice():
-
-    import sys
-
-    class MenuError(Exception):
-        pass
-
-    class HiddenMenu(Exception):
-        pass
-
-    option = ''
-    # User enters option number, its validated as an integer and passes to the Options List
-    while True:
-
-        try:
-            option = int(input('Please Choose an Option : '))
-            if 1 <= option <= 5:
-                break
-            elif option == 1729:
-                raise HiddenMenu()
-            else:
-                raise MenuError()
-
-        except ValueError:
-            print('Please enter a valid option, whole numbers please.')
-            continue
-        except MenuError:
-            print('Please enter a valid option, ' + str(option) + " isn't an option.")
-        except HiddenMenu:
-            print('Very Clever, but nothing is hidden. Yet')
-
-    if 1 <= option <= 5:
-        # Options List
-        if option == 1:
-            debugkeysign()
-        elif option == 2:
-            privatekeysign()
-        elif option == 3:
-            zipalign()
-        elif option == 4:
-            genprivkey()
-        elif option == 5:
-            # Exit gracefully
-            print('Quitting')
-            sys.exit()
-    print('Please Choose a Valid Option')
-
-
-def checkoutputfolder():
-
-    import os
-    import os.path
-
-    # Create Required Folders
-    if not os.path.isdir('./SignedApks/'):
-        os.mkdir('SignedApks')
-    if not os.path.isdir('./ZipAlignedApks/'):
-        os.mkdir('ZipAlignedApks')
-    if not os.path.isdir('./PrivateKey/'):
-        os.mkdir('PrivateKey')
-    mainmenu()
+# Loads the splash, with a standard Apache Licence 2.0 disclaimer, and an acceptance option
 
 
 def splash():
@@ -143,11 +46,130 @@ limitations under the License.''')
     print()
     lic = input('Do you accept the terms of the above licence? Y/N - ')
     print()
+    #
     if lic.lower() == 'y':
+        del lic
         checkjava()
     else:
-        print('Quitting')
+        print('Without accepting the terms, you cannot continue')
+        input('Press Enter To Exit')
+        del lic
         sys.exit()
+
+
+def checkjava():
+
+    import sys
+    import subprocess
+    # Java is called, checking if its in the system path, if not. It should exit with a user prompt
+    # Not fully tested, java is installed on all my machines.
+    sjava = subprocess.call(['java', '-version'])
+    if sjava is None:
+        print('Java not installed or added to system path, please install')
+        input('Press Enter To Exit')
+        del sjava
+        # Exit gracefully
+        sys.exit()
+    else:
+        print()
+        del sjava
+        # Moves on to the output folder checking function
+        checkoutputfolder()
+
+
+def checkoutputfolder():
+
+    import os
+    import os.path
+
+    # Create required output folders
+    if not os.path.isdir('./SignedApks/'):
+        os.mkdir('SignedApks')
+    if not os.path.isdir('./ZipAlignedApks/'):
+        os.mkdir('ZipAlignedApks')
+    if not os.path.isdir('./PrivateKey/'):
+        os.mkdir('PrivateKey')
+    # All clear, now the main menu function is loaded.
+    mainmenu()
+
+
+def mainmenu():
+
+    # Here the main menu is printed
+    linebreak = ('-' * 28)
+    print('Main Menu')
+    print(linebreak)
+    print('1 : Sign With Debug Key')
+    print('2 : Sign With Private Key')
+    print('3 : ZipAlign Signed APKs')
+    print(linebreak)
+    print('4 : Generate Private Key')
+    print(linebreak)
+    print('5 : Exit ')
+    print(linebreak)
+    del linebreak
+    # Runs the menu choice function
+    menuchoice()
+
+
+def menuchoice():
+
+    import sys
+
+    # A Couple custom exceptions are created to catch any invalid/secret menu choices
+    class MenuError(Exception):
+        pass
+
+    class HiddenMenu(Exception):
+        pass
+
+    option = ''
+    # User enters option number, its validated as an integer and passes to the Options List
+    while True:
+        # This while loop catches the option choice and will only break if the choice is valid
+        try:
+            option = int(input('Please Choose an Option : '))
+            if 1 <= option <= 5:
+                break
+            elif option == 1729:
+                raise HiddenMenu()
+            else:
+                raise MenuError()
+        # If the option choice is not a valid integer or a character this exception is raised
+        except ValueError:
+            print('Please enter a valid option, whole numbers please.')
+            continue
+        # If a valid integer is entered, but not a valid menu option, this exception is raised
+        except MenuError:
+            print('Please enter a valid option, ' + str(option) + " isn't an option.")
+        # If 1729 is entered, this exception is raised.
+        # Ref: Srinivasa Ramanujan - http://en.wikipedia.org/wiki/Srinivasa_Ramanujan
+        except HiddenMenu:
+            print('Very Clever, but nothing is hidden. Yet')
+
+    # Menu choice logic
+    if 1 <= option <= 5:
+        # Options List
+        if option == 1:
+            del option
+            debugkeysign()
+        elif option == 2:
+            del option
+            privatekeysign()
+            del option
+        elif option == 3:
+            del option
+            zipalign()
+        elif option == 4:
+            del option
+            genprivkey()
+        elif option == 5:
+            del option
+            # Exit gracefully
+            print('Quitting')
+            sys.exit()
+    print('Please Choose a Valid Option : ')
+
 
 def debugkeysign():
 
@@ -164,9 +186,8 @@ def debugkeysign():
     # If none are found exit with message
     if len(apklist) == 0:
         print('No APKs Found' + '\n')
-        pause = input('Press Enter to Return to Menu')
+        input('Press Enter to Return to Menu')
         print()
-        del pause
         mainmenu()
     else:
         print('Found ' + str(len(apklist)) + ' APK' + '\n')
@@ -184,6 +205,7 @@ def debugkeysign():
     print('Signing has finished, please check the messages above for any errors.')
     input('Press Enter to continue')
     print()
+    del apklist, apkdir, debugkeydir, debugkeys
     mainmenu()
 
 
@@ -203,20 +225,29 @@ def privatekeysign():
     # Returns the number of APKs Found
     if len(apklist) == 0:
         print('No APKs Found' + '\n')
-        pause = input('Press Enter to Return to Menu')
+        input('Press Enter to Return to Menu')
         print()
-        del pause
         mainmenu()
     else:
         usralias = input('Alias You Wish To Sign With : ')
         apkcount = len(apklist)
         signcount = 0
-        print('Found ' + str(apkcount) + ' APK' + '\n')
+        if len(apklist) == 1:
+            print('Found ' + str(apkcount) + ' APK' + '\n')
+        else:
+            print('Found ' + str(apkcount) + ' APKs' + '\n')
+        # Insert warning about entering the keystore password
+        print('Important, your KEYSTORE password is required to sign multiple apks at once.')
+        print("Your KEYSTORE password is not used outside of this menu option. It's cleared when signing is complete.")
+        print()
         keystorepass = input('Please Enter your KEYSTORE Password : ')
         while signcount != apkcount:
             for APK in apklist:
                 print('Signing ' + APK)
+                # Because private key signing is done "in-place" we backup the original apk, sign, move and restore
+                # Create tmp folder
                 os.mkdir('tmp')
+                # Copy unsigned apk out
                 shutil.copyfile('./UnsignedApks/' + APK, './tmp/' + APK)
                 subprocess.call(['./Files/jarsigner.exe',
                                  '-keystore',
@@ -229,18 +260,36 @@ def privatekeysign():
                                  #'-verbose',
                                  #'-certs'
                 ])
+                # Now we try to put the signed apk into the signed folder
                 try:
                     os.rename('./UnsignedApks/' + APK, './SignedApks/' + APK)
                 except FileExistsError:
+                    # If the file already exists, we move signed file out and replace
                     os.rename('./SignedApks/' + APK, './tmp/T_' + APK)
                     os.rename('./UnsignedApks/' + APK, './SignedApks/' + APK)
+                except PermissionError:
+                    # If the unsigned apk is open, this error is raised
+                    print()
+                    print('Seems ' + APK + ' is open elsewhere. Unable to continue')
+                    input('Press Enter to Quit to Menu')
+                    print()
+                    mainmenu()
+                # Restore the unsigned apk.
                 os.rename('./tmp/' + APK, './UnsignedApks/' + APK)
+                # Clean up
                 shutil.rmtree('./tmp/')
                 signcount += 1
         print()
         print('Signing has finished, please check the messages above for any errors.')
+        # Keystore password is overwritten, then deleted. Alias is also deleted
+        keystorepass = '_0_'
+        del keystorepass, usralias
+        print('Your KEYSTORE password has been cleared')
         input('Press Enter to continue')
         print()
+        # Clear other objects
+        del apkdir, apklist, apkcount, signcount
+        # Return to the menu
         mainmenu()
 
 
@@ -248,9 +297,24 @@ def genprivkey():
 
     import subprocess
 
-    print('Insert Warning')
+    # Generic warning about trusting some code from a random bloke on the intertubes
+    print('''
+    WARNING
+    -------
+
+    You are about to generate a private key to sign (potentially) your own apps.
+    You are doing this using software that you have not authored yourself.
+    Exercise extreme caution when inputting passwords and/or aliases.
+    If you did not download this from XDA-Developers, be even more cautious.
+
+    If you wish full source is available to download, inspect & compare.
+    https://github.com/andyharney/pySignare
+    ''')
+    print()
+    # Grab some info from the user to smooth out the key generation
     usralias = input('Choose Desired Alias : ')
-    keypass = input('Choose Desired KEY (not keystore) Password : ')
+    keypass = input('Choose a Desired KEY (not keystore) Password : ')
+    # Keypass has to be at least 6 chars long, its re-entered until it is.
     while len(keypass) < 6:
         print('Your Key Password MUST be greater than 6 characters, try again')
         keypass = input('Choose Desired KEY Password : ')
@@ -271,6 +335,12 @@ def genprivkey():
                      '10000'
     ])
     print()
+    print('Seems your key was created successfully')
+    input('Press Enter to Continue')
+    # Overwrite keypass
+    keypass = '_0_'
+    # Delete the keypass & usralias objects
+    del usralias, keypass
     mainmenu()
 
 
@@ -286,8 +356,7 @@ def zipalign():
     apkcount = len(apklist)
     if len(apklist) == 0:
         print('No Signed APKs Found' + '\n')
-        pause = input('Press Enter to Return to Menu')
-        del pause
+        input('Press Enter to Return to Menu')
         print()
         mainmenu()
     else:
@@ -303,8 +372,10 @@ def zipalign():
             zipaligncount += 1
     print()
     print('Zip Aligning has finished, please check the messages above for any errors.')
-    input('Press Enter to continue')
+    input('Press Enter to Continue')
     print()
+    # Clear out the objects
+    del zipaligncount, apkdir,  apklist, apkcount
     mainmenu()
 
 splash()
